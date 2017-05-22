3d0ea70b01df02d17aabe7498e3a9905083e4777
Wewannado <rogilico@gmail.com>
2017-05-22 19:36:29 +0200

    Parcialmente completado

--- src/domino/vista/Tauler.java
+++ src/domino/vista/Tauler.java
@@ -6,6 +6,8 @@ package domino.vista;
 import domino.model.Fitxa;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.ComponentOrientation;
+import java.awt.FlowLayout;
 import java.util.ArrayDeque;
 import java.util.List;
 import javax.swing.BoxLayout;
@@ -41,7 +43,12 @@ public class Tauler extends javax.swing.JPanel {
         eastJpanel.setBackground(this.getBackground());
         eastJpanel.setLayout(new BoxLayout(eastJpanel, BoxLayout.Y_AXIS));
         JPanel southJpanel = new JPanel();
+        southJpanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
         southJpanel.setBackground(this.getBackground());
+        southJpanel.applyComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        JPanel westJpanel = new JPanel();
+        westJpanel.setBackground(this.getBackground());
+        westJpanel.setLayout(new BoxLayout(westJpanel, BoxLayout.Y_AXIS));
         for (Fitxa fitxe : fitxes) {
             BotoFitxa fitxa = new BotoFitxa();
             fitxa.setValue(fitxe.getValors());
@@ -55,8 +62,8 @@ public class Tauler extends javax.swing.JPanel {
                 fitxa.setFocusPainted(false);
                 fitxa.setOpaque(false);
                 northJpanel.add(fitxa);
-                
-            } else if (count <12){
+
+            } else if (count < 12) {
                 ImageIcon icon = new ImageIcon("assets/" + fitxe.getValors()[0] + fitxe.getValors()[1] + "v.png");
                 fitxa.setIcon(icon);
                 fitxa.setBorderPainted(false);
@@ -64,23 +71,31 @@ public class Tauler extends javax.swing.JPanel {
                 fitxa.setFocusPainted(false);
                 fitxa.setOpaque(false);
                 eastJpanel.add(fitxa);
-            }
-            else{
-                ImageIcon icon = new ImageIcon("assets/" + fitxe.getValors()[0] + fitxe.getValors()[1] + ".png");
+            } else if (count < 18) {
+                ImageIcon icon = new ImageIcon("assets/" + fitxe.getValors()[1] + fitxe.getValors()[0] + ".png");
                 fitxa.setIcon(icon);
                 fitxa.setBorderPainted(false);
                 fitxa.setContentAreaFilled(false);
                 fitxa.setFocusPainted(false);
                 fitxa.setOpaque(false);
                 southJpanel.add(fitxa);
+            } else {
+                ImageIcon icon = new ImageIcon("assets/" + fitxe.getValors()[0] + fitxe.getValors()[1] + "v.png");
+                fitxa.setIcon(icon);
+                fitxa.setBorderPainted(false);
+                fitxa.setContentAreaFilled(false);
+                fitxa.setFocusPainted(false);
+                fitxa.setOpaque(false);
+                westJpanel.add(fitxa);
             }
-            
+
             count++;
 
         }
         this.add(northJpanel, BorderLayout.NORTH);
         this.add(eastJpanel, BorderLayout.EAST);
         this.add(southJpanel, BorderLayout.SOUTH);
+        this.add(westJpanel, BorderLayout.WEST);
         this.revalidate();
         this.repaint();
     }
